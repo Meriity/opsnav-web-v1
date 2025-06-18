5e8244d6d4283592b1274173453d652bf8372a92
Subhajit Bokshi <52731122+Subhajitb17@users.noreply.github.com>
2025-06-18 18:07:54 +0530

    Initialize React frontend with Vite

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@pages': path.resolve(__dirname, './src/pages')
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/assets/styles/_variables.scss";`
+      }
+    }
+  }
+})
