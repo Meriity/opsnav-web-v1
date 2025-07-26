7a8636f8df5a05ac4d9369e753954a7899f549c8
Jeganath18 <itzjeganhere@gmail.com>
2025-07-26 11:40:14 +0530

    Changes in settlement date modal.

--- src/components/ui/DateRangeModal.jsx
+++ src/components/ui/DateRangeModal.jsx
@@ -49,12 +49,16 @@ export default function DateRangeModal({ isOpen, setIsOpen, subTitle = "", hande
                         <p className="text-sm text-gray-600 mb-5">{subTitle}</p>
 
                         <div className="space-y-4">
+                            <label className="block text-sm font-medium text-gray-700">From Date</label>
                             <input
                                 type="date"
                                 value={fromDate}
                                 onChange={(e) => setFromDate(e.target.value)}
                                 className="w-full px-4 py-2 border rounded"
                             />
+                        </div>
+                        <div className="space-y-4 mt-4">
+                            <label className="block text-sm font-medium text-gray-700">To Date</label>
                             <input
                                 type="date"
                                 value={toDate}
