f15360b9b43912bcb23aee58ccdbcd3433901d12
Jeganath18 <jeganb007@gmail.com>
2025-07-31 10:38:34 +0530

    Completely redesigned Client Dashboard
    Add a spinner while login in Admin Login
    
    Add a spinner while login in client Login

--- src/components/auth/AdminLogin.jsx
+++ src/components/auth/AdminLogin.jsx
@@ -47,7 +47,7 @@ function LoginForm() {
 
       <div className="max-w-6xl w-full px-6 flex flex-col md:flex-row items-center justify-between">
         <div className="w-full md:w-1/2 text-center md:text-left mb-8 md:mb-0">
-          <img src="/Logo.png" alt="VK Lawyers Logo" className="h-8 mx-auto sm:h-8 md:h-12 md:mx-0 lg:h-16" />
+          <img src="/Logo.png" alt="VK Lawyers Logo" className="h-24 mx-auto md:mx-0" />
           <h1 className="text-3xl font-bold mt-4 font-poppins md:text-2xl sm:text-xl">WELCOME TO OPSNAV</h1>
           <p className="text-gray-600 mt-2 font-poppins">
             Secure. Simple. Seamless conveyancing.
@@ -85,7 +85,33 @@ function LoginForm() {
               disabled={isLoading}
               className="w-full bg-sky-600 text-white py-2 rounded-md hover:bg-sky-700 transition cursor-pointer"
             >
-              {isLoading ? "Processing..." : "Sign In"}
+              {isLoading ? (
+  <span className="flex items-center justify-center gap-2">
+    <svg
+      className="w-4 h-4 animate-spin text-white"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <circle
+        className="opacity-25"
+        cx="12"
+        cy="12"
+        r="10"
+        stroke="currentColor"
+        strokeWidth="4"
+      ></circle>
+      <path
+        className="opacity-75"
+        fill="currentColor"
+        d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
+      ></path>
+    </svg>
+    Signing in...
+  </span>
+) : (
+  "Sign In"
+)}
             </button>
             {error && (
               <div className="bg-red-100 text-red-700 p-2 mb-4 rounded text-sm">
