6b62125aeef37a6c337f7597c22d8197cf352029
Jeganath18 <jeganb007@gmail.com>
2025-09-11 00:47:48 +0530

    Fix minor div glitch

--- src/components/ui/CreateClientModal.jsx
+++ src/components/ui/CreateClientModal.jsx
@@ -454,7 +454,7 @@ export default function CreateClientModal({ isOpen, setIsOpen, companyName, crea
               <label className="block mb-1 font-medium">ABN</label>
               <input type="text" value={formData.abn} className="w-full px-4 py-2 rounded-md border border-gray-300 bg-white text-gray-600" />
             </div>
-              </>
+              </div>
             )}
 
             {companyName === "idg" && createType === "order" && (
