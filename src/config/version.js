097c8c4b90b90cb9f487ec5d9cdf36a86e11d0dc
Varunvasan1412 <varunvasan1412@gmail.com>
2026-02-25 10:33:30 +0530

    Client dashboard greetings messge updated to show only first name

--- src/config/version.js
+++ src/config/version.js
@@ -2,4 +2,4 @@
  * Application Version Configuration
  * Update this file to change the version number across the entire application.
  */
-export const APP_VERSION = "v6.6.1";
+export const APP_VERSION = "v6.6.2";
